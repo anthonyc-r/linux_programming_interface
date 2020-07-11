f7ecbe3cee0cfb152b0d899ceed5bdbc77f43aab
anthony <anthony@localhost>
2020-07-11 15:56:47 +0100

    setfattr

--- setfattr.c
+++ setfattr.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <sys/xattr.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 	char opt, *name, *value, *file;
@@ -9,16 +11,24 @@ int main(int argc, char **argv) {
 	while ((opt = getopt(argc, argv, "n:v:")) != -1) { 
 		switch (opt) {
 			case 'n':
-				name = optarg;
+				name = malloc(strlen(optarg) + 5);
+				sprintf(name, "user.%s", optarg);
 				break;
 			case 'v':
 				value = optarg;
 				break;
 		}
 	}
+	if (optind >= argc) {
+		printf("Usage ./setfattr -n name -v value file\n");
+		exit(EXIT_FAILURE);
+	}	
 	file = argv[optind];
-	
-	printf("name: %s, value: %s file: %s\n", name, value, file);
+	printf("value: %s\n", value);
+	if (setxattr(file, name, value, strlen(value), 0) == -1) {
+		printf("setxattr\n");
+		exit(EXIT_FAILURE);
+	}
 	
 	exit(EXIT_SUCCESS);
 }
