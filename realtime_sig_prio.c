18a8cf5657fe768da812c80f0e8ca8fde95695fc
anthony <anthony@localhost>
2020-10-03 10:19:09 +0100

    realtime signal priority

--- realtime_sig_prio.c
+++ realtime_sig_prio.c
@@ -3,12 +3,43 @@
 * like a priority level. However, it is not standardized as to whether standard
 * signals are delivered before realtime signals, or vice versa.
 * This program demonstrates whether realtime or standard signals are delivered
-* first on the host system. (Actually OpenBSD at the time of writing).
+* first on the host system.
 *
 * This program first blocks all signals, during which time the user should send
 * various realtime and standard signals to this process. Afterwards, send a
 * SIGINT to unblock all signals. The program will print out signals as they are
 * received, and then terminate. SIGQUIT will terminate the program.
+*
+* Example session:
+* $ ./realtime &
+* [3] 58810
+* ubuntu@ubuntu:~/workspace$ Realtime signals are from 34 to 64
+* $ kill -USR1 58810 
+* $ kill -USR1 58810 
+* $ kill -USR1 58810 
+* $ kill -USR2 58810 
+* $ kill -USR2 58810 
+* $ kill -USR2 58810 
+* $ kill -USR2 58810 
+* $ kill -35 58810 
+* $ kill -37 58810 
+* $ kill -40 58810 
+* $ kill -44 58810 
+* $ kill -INT 58810 
+* Unblocking all signals
+* Received signal: User defined signal 1
+* $ Received signal: User defined signal 2
+* Received signal: Unknown signal 35
+* Received signal: Unknown signal 37
+* Received signal: Unknown signal 40
+* Received signal: Unknown signal 44
+* kill -QUIT 58810 
+* Finishing
+*
+* From the above it looks like standard signals are delivered before realtime.
+* Note that if I were to leave sa_mask as empty, the above would be very
+* confusingly reversed. This is because each signal handler gets interuppted by the
+* next, before it reaches the print statement!
 */
 
 #include <unistd.h>
@@ -45,11 +76,13 @@ int main(int argc, char **argv) {
 	sigfillset(&block);
 	sigdelset(&block, SIGINT);
 	sigdelset(&block, SIGQUIT);
-	
 	sigprocmask(SIG_SETMASK, &block, NULL);
-	sigemptyset(&sigact.sa_mask);
-	sigact.sa_flags = 0;
 	
+	// Note that if we leave this as empty set, we will appear to get
+	// signals in reverse order! As each signal will get interupted by the
+	// next, before the printf is reached in the sig handler!!
+	sigfillset(&sigact.sa_mask);
+	sigact.sa_flags = 0;
 	sigact.sa_handler = all_handler;
 	for (sig = 1; sig < NSIG; sig++) {
 		sigaction(sig, &sigact, NULL);
