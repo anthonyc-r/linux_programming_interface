ffdd0be5e5578cdf75bb5d89dd6b8dcf6e9b1984
anthony <anthony@localhost>
2021-02-13 10:19:24 +0000

    fifo sequence server - add last number backup

--- num_server/num_server.c
+++ num_server/num_server.c
@@ -14,24 +14,74 @@
 #include <errno.h>
 #include <string.h>
 #include <limits.h>
+#include <sys/stat.h>
 
 #define REQ_SZ sizeof (struct num_open_req)
+#define BAK_FILE "/tmp/num_server.bak"
+#define FILE_PERMS S_IWUSR | S_IRUSR | S_IWOTH | S_IROTH
 
 static void exit_err(char *reason) {
 	printf("%s (%s)\n", reason, strerror(errno));
 	exit(EXIT_FAILURE);
 }
 
+static int load_backup(int *seq_start) {
+	int fd, flags;
+	struct stat sbuf;
+	off_t off;
+
+	*seq_start = 0;
+	flags = O_RDWR | O_SYNC;
+
+	if (stat(BAK_FILE, &sbuf) == -1) {
+		puts("stat -1");
+		flags |= O_CREAT;
+	}
+	if ((fd = open(BAK_FILE, flags, FILE_PERMS)) == -1)
+		exit_err("open backup file");
+	// New file, starts at 0.
+	if (flags & O_CREAT) {
+		puts("Creating new backup file");
+		if (write(fd, seq_start, sizeof (int)) != sizeof (int))
+			exit_err("write init backup");
+		return fd;
+	}
+
+	// Make sure the existing file is valid.
+	if (lseek(fd, 0, SEEK_END) != sizeof (int)) {
+		exit_err("lseek invalid backup file");
+	}
+
+	// Ok.
+	off = lseek(fd, 0, SEEK_SET);
+	if (off == (off_t) - 1)
+		exit_err("lseek 2");
+	if (read(fd, seq_start, sizeof (int)) != sizeof (int))
+		exit_err("read backup");
+	printf("backup is %d\n", *seq_start);
+	return fd;
+}
+
+static void update_backup(int fd, int seq_start) {
+	if (lseek(fd, 0, SEEK_SET) == (off_t) -1)
+		exit_err("lseek 3");
+	printf("bakfd: %lld\n", (long long)fd);
+	if (write(fd, &seq_start, sizeof (int)) != sizeof (int))
+		exit_err("write backup");
+}
 
 int main(int argc, char **argv) {
 	int n, i;
-	int sfifo, cfifo, sfifo_write;
+	int sfifo, cfifo, sfifo_write, bak;
 	char cpath[PATH_MAX];
 	ssize_t nread, res_sz;
 	struct num_open_req req;
 	struct num_response *res;
 
-	if (mkfifo(FIFO_SERVER, S_IWUSR | S_IRUSR | S_IWOTH | S_IROTH) == -1)
+	bak = load_backup(&n);
+	
+	unlink(FIFO_SERVER);
+	if (mkfifo(FIFO_SERVER, FILE_PERMS) == -1)
 		exit_err("mkfifo");
 	if ((sfifo = open(FIFO_SERVER, O_RDONLY)) == -1)
 		exit_err("open 1");
@@ -41,7 +91,6 @@ int main(int argc, char **argv) {
 		exit_err("open 2");
 
 	errno = 0;
-	n = 0;
 	while ((nread = read(sfifo, &req, REQ_SZ)) == REQ_SZ) {
 		printf("Got request - pid: %lld, nreq: %d\n", (long long)req.pid, req.nreq);
 		res_sz = (sizeof (struct num_response)) + req.nreq * sizeof (int);
@@ -62,10 +111,12 @@ int main(int argc, char **argv) {
 		free(res);
 		if (close(cfifo) == -1)
 			exit_err("close 1");
+		update_backup(bak, n);
 	}
 	if (nread != 0)
 		exit_err("read 1");
-
+	
+	close(bak);
 	close(sfifo_write);
 	close(sfifo);
 	unlink(FIFO_CLIENT_TEMPLATE);
