b3e168d17900f136df5624b63612ead1e85fcaee
anthony <anthonyc@gmx.co.uk>
2022-02-27 08:47:26 +0000

    Convert rt_sigio to use realtime signals and print info in the handler

--- rt_sigio.c
+++ rt_sigio.c
@@ -3,6 +3,9 @@
  * signal driven io.
  */
 
+// For F_SETSIG
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
@@ -10,6 +13,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
+
+#define RTSIG ( SIGRTMIN )
 
 static volatile sig_atomic_t signalled;
 static struct termios *tios_reset;
@@ -65,16 +71,44 @@ static void exit_err(char *reason) {
 	exit(EXIT_FAILURE);
 }
 
-static void handle(int sig) {
+static void handle(int sig, siginfo_t *info, void *ucontext) {
+	char *code;
+
 	signalled = 1;
+	switch (info->si_code) {
+		case POLL_IN:
+			code = "POLL_IN";
+			break;
+		case POLL_OUT:
+			code = "POLL_OUT";
+			break;
+		case POLL_MSG:
+			code = "POLL_MSG";
+			break;
+		case POLL_ERR:
+			code = "POLL_ERR";
+			break;
+		case POLL_PRI:
+			code = "POLL_PRIO";
+			break;
+		case POLL_HUP:
+			code = "POLL_HUP";
+			break;
+		default:
+			code = "UNKNOWN";
+	}
+	// Not async signal safe...
+	printf("si_fd: %d\n", info->si_fd);
+	printf("si_code: %s\n", code);
 }
 
 static int setup_handler() {
 	struct sigaction sigact;
 	memset(&sigact, 0, sizeof(sigact));
-	sigact.sa_handler = handle;
+	sigact.sa_sigaction = handle;
+	sigact.sa_flags = SA_SIGINFO;
 	sigemptyset(&sigact.sa_mask);
-	return sigaction(SIGIO, &sigact, NULL);
+	return sigaction(RTSIG, &sigact, NULL);
 }
 
 int main(int argc, char **argv) {
@@ -91,6 +125,10 @@ int main(int argc, char **argv) {
 
 	if (setup_handler() == -1)
 		exit_err("setup handler");
+
+	// Set this before async to avoid potentially getting a SIGINFO.
+	if (fcntl(STDIN_FILENO, F_SETSIG, RTSIG) == -1)
+		exit_err("Setup realtime signal");
 
 	if (set_async(STDIN_FILENO) == -1)
 		exit_err("set async");
