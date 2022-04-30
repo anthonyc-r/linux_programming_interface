e5f070ba184aa253ea57c03a6051236009ae9178
anthony <anthony@localhost>
2022-04-30 08:53:41 +0100

    pty based rsh/rlogin working

--- rsh_pty.c
+++ rsh_pty.c
@@ -18,12 +18,39 @@
 #include <sys/sendfile.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <utmpx.h>
+#include <termios.h>
+#include <signal.h>
 
 #define BUFSZ 100
 
+static struct termios tios_restore;
+
 static void exit_err(char *reason) {
-	perror("socket");
+	perror(reason);
 	exit(EXIT_FAILURE);
+}
+
+static void restore_tios() {
+	tcsetattr(STDOUT_FILENO, TCSADRAIN, &tios_restore);
+}
+
+static void goraw() {
+	struct termios raw;
+
+	if (tcgetattr(STDOUT_FILENO, &tios_restore) == -1)
+		exit_err("tcgetattr");
+	if (atexit(restore_tios) != 0)
+		exit_err("atexit");
+	raw = tios_restore;
+	raw.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
+	raw.c_iflag &= ~(BRKINT | ICRNL | IGNBRK | IGNCR | INLCR |
+		INPCK | ISTRIP | IXON | PARMRK);
+	raw.c_oflag &= ~OPOST;
+	raw.c_cc[VMIN] = 1;
+	raw.c_cc[VTIME] = 0;
+	if (tcsetattr(STDOUT_FILENO, TCSADRAIN, &raw) == -1)
+		exit_err("tcsetattr raw");
 }
 
 static void pass_through(int a, int b) {
@@ -39,12 +66,12 @@ static void pass_through(int a, int b) {
 	polls[FDB].events = POLLIN;
 	while ((nread = poll(polls, 2, -1)) > 0) {
 		if (polls[FDA].revents & POLLIN == POLLIN) {
-			if ((nread = read(a, buf, BUFSZ)) == -1)
+			if ((nread = read(a, buf, BUFSZ)) < 1)
 				exit_err("read fd");
 			if (write(b, buf, nread) != nread)
 				exit_err("write master");
 		} else if (polls[FDB].revents & POLLIN == POLLIN) {
-			if ((nread = read(b, buf, BUFSZ)) == -1)
+			if ((nread = read(b, buf, BUFSZ)) < 1)
 				exit_err("read master");
 			if (write(a, buf, nread) != nread)
 				exit_err("write fd");
@@ -83,12 +110,29 @@ static void client(char *host, char *port, char *cmd) {
 	if (curs == NULL)
 		exit_err("no usable adr found");
 	freeaddrinfo(res);
+	goraw();
 	pass_through(sock, STDOUT_FILENO);
+}
+
+
+static int serve_clientfd;
+static void handle(int sig, siginfo_t *info, void *ctx) {
+	close(serve_clientfd);
+}
+static void setup_sigchild() {
+	struct sigaction sigact;
+	sigact.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_restorer = NULL;
+	sigact.sa_sigaction = handle;
+	if (sigaction(SIGCHLD, &sigact, NULL) == -1)
+		exit_err("sigaction"); 
 }
 
 static void serve(int fd, int sock) {
 	int master, slave, nready;
 	char *snam;
+	uid_t suid;
 
 	if ((master = posix_openpt(O_RDWR)) == -1)
 		exit_err("posix_openpt");
@@ -98,8 +142,6 @@ static void serve(int fd, int sock) {
 		exit_err("granpt");
 	if ((snam = ptsname(master)) == NULL)
 		exit_err("ptsname");
-	if ((slave = open(snam, O_RDWR)) == -1)
-		exit_err("open slave");
 
 	switch (fork()) {
 	case -1:
@@ -107,16 +149,26 @@ static void serve(int fd, int sock) {
 		break;
 	case 0:
 		close(sock);
+		setsid();
+		if ((slave = open(snam, O_RDWR)) == -1)
+			exit_err("open slave");
 		dup2(slave, STDOUT_FILENO);
 		dup2(slave, STDERR_FILENO);
 		dup2(slave, STDIN_FILENO);
+		seteuid(0);
+		// Just doing seteuid isn't enough, and indeed, results in a very confusing error from login...
+		setuid(0);
+		if (getuid() != 0)
+			exit_err("setuid fail. Please run this program as setuid");
 		execl("/bin/login", "login", (char*)NULL);
 		perror("execve");
-		exit(EXIT_FAILURE);
 	default:
 		break;
 	}
 	close(slave);
+	// Want to close the client when login exits.
+	serve_clientfd = fd;
+	setup_sigchild();
 	pass_through(fd, master);
 	close(fd);
 	close(master);
@@ -156,18 +208,31 @@ static void server() {
 
 	while ((client = accept(sock, NULL, NULL)) != -1) {
 		puts("Accepting client");
-		serve(client, sock);
+		switch (fork()) {
+		case -1:
+			exit_err("fork");
+		case 0:
+			serve(client, sock);
+			exit(EXIT_SUCCESS);
+		default:
+			close(client);
+			break;
+		}
 	}
 	close(sock);
 	closelog();
 }
 
 int main(int argc, char **argv) {
+	seteuid(getuid());
 	if (argc > 1 && strcmp("server", argv[1]) == 0) {
 		server();
 	} else if (argc > 3 && strcmp("client", argv[1]) == 0) {
+		// Also sets saved uid as per manpage
+		setreuid(getuid(), getuid());
 		client(argv[2], argv[3], argv[4]);
 	} else {
+		setreuid(getuid(), getuid());
 		puts("Usage: ./rsh (server|client <host> <port>)");
 		exit(EXIT_FAILURE);
 	}
